dc1e8adb8cadb7f6c5255ebfce800ca67ebc844c
beautriceinko-crypto <beautriceinko@gmail.com>
2026-09-06 10:41:23 +0100

    Fix SafeAreaProvider import causing startup crash

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import {AppRegistry} from 'react-native';
-import {SafeAreaProvider} from '@react-native-safe-area-context';
+import {SafeAreaProvider} from 'react-native-safe-area-context';
 import App from './App';
 import {name as appName} from './app.json';
 
