85438d8d9a1c094d2b41357329cf50ddfb0d63dc
beautriceinko-crypto <beautriceinko@gmail.com>
2026-09-06 10:15:17 +0100

    Add the React Native application host

---
+++ android/app/src/main/java/com/beatrice/liquidglasslauncher/MainApplication.kt
@@ -0,0 +1,24 @@
+package com.beatrice.liquidglasslauncher
+
+import android.app.Application
+import com.facebook.react.PackageList
+import com.facebook.react.ReactApplication
+import com.facebook.react.ReactHost
+import com.facebook.react.ReactNativeApplicationEntryPoint.loadReactNative
+import com.facebook.react.defaults.DefaultReactHost.getDefaultReactHost
+
+class MainApplication : Application(), ReactApplication {
+    override val reactHost: ReactHost by lazy {
+        getDefaultReactHost(
+            context = applicationContext,
+            packageList = PackageList(this).packages.apply {
+                // Native packages are autolinked by the React Native Gradle plugin.
+            },
+        )
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+        loadReactNative(this)
+    }
+}
